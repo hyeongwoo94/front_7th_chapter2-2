0e6f2240093d58f5795ea259588fb294501430a3
hyeongwoo <guddn0625@gmail.com>
2025-11-19 09:19:19 +0900

    페이지 배포

--- createViteConfig.ts
+++ createViteConfig.ts
@@ -2,15 +2,26 @@ import { defineConfig as defineTestConfig, mergeConfig, ViteUserConfig } from "v
 import { defineConfig, UserConfig } from "vite";
 
 export function createViteConfig(options: UserConfig = {}, testOptions: ViteUserConfig["test"] = {}): UserConfig {
-  return mergeConfig(
-    defineConfig(options),
-    defineTestConfig({
-      test: {
-        globals: true,
-        environment: "jsdom",
-        exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
-        ...testOptions,
-      },
-    }),
-  );
+  const config = defineConfig(options);
+  const testConfig = defineTestConfig({
+    test: {
+      globals: true,
+      environment: "jsdom",
+      exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
+      ...testOptions,
+    },
+  });
+
+  const merged = mergeConfig(config, testConfig);
+
+  if (options.define && merged.define) {
+    merged.define = {
+      ...merged.define,
+      ...options.define,
+    };
+  } else if (options.define) {
+    merged.define = options.define;
+  }
+
+  return merged;
 }
